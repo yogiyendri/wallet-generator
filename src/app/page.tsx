3b19b5565886b0681f89d9d1e02894088bfd35e2
yogiyendri <yogiyendrisaputra@gmail.com>
2023-10-05 22:08:03 +0700

    first-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
 
-export default function Home() {
+export default function App() {
+  const [address, setAddress] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
+  const [pharse, setPharse] = useState("");
+  const { toast } = useToast();
+
+  function copyText(entryText: string) {
+    navigator.clipboard.writeText(entryText);
+    return console.log("copied");
+  }
+
+  function generateWallet() {
+    const getmnemonic: any = ethers.HDNodeWallet.createRandom().mnemonic;
+    const getWallet = ethers.HDNodeWallet.fromMnemonic(getmnemonic);
+    setAddress(getWallet.address);
+    setPrivateKey(getWallet.privateKey);
+    setPharse(getWallet.mnemonic?.phrase ?? "");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-auto h-screen max-w-7xl">
+      <div className="flex flex-col md:flex-row h-full items-center justify-between">
+        <div className="w-full max-w-lg">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-gray-900 lg:text-5xl">
+            EVM Wallet Generator
+          </h1>
+          <p className="mt-4 text-sm text-muted-foreground">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            maxime facere placeat natus inventore impedit in voluptatum hic
+            animi est.
+          </p>
+          <Button onClick={generateWallet} className="mt-6">
+            Generate
+          </Button>
+        </div>
+        <div className="w-full ml-0 lg:ml-16">
+          <h2 className="text-lg font-semibold">Result</h2>
+          <div>
+            <Label htmlFor="walletAddress">Wallet Address</Label>
+            <div className="flex w-full items-center space-x-2">
+              <Input id="walletAddress" readOnly value={address} />
+              <Button
+                onClick={() => {
+                  copyText(address);
+                  toast({ description: "Copied to clipboard." });
+                }}
+                variant={"secondary"}
+              >
+                Copy
+              </Button>
+            </div>
+          </div>
+          <div>
+            <Label htmlFor="privateKey">Private Key</Label>
+            <div className="flex w-full items-center space-x-2">
+              <Input id="privateKey" readOnly value={privateKey} />
+              <Button
+                onClick={() => {
+                  copyText(privateKey);
+                  toast({ description: "Copied to clipboard." });
+                }}
+                variant={"secondary"}
+              >
+                Copy
+              </Button>
+            </div>
+          </div>
+          <div>
+            <Label htmlFor="mnemonic">Mnemonic</Label>
+            <div className="flex w-full items-center space-x-2">
+              <Input id="mnemonic" readOnly value={pharse} />
+              <Button
+                onClick={() => {
+                  copyText(pharse);
+                  toast({ description: "Copied to clipboard." });
+                }}
+                variant={"secondary"}
+              >
+                Copy
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
